3095b679ced1ca7b7e662ccf9970da62603544d9
sergio mac <s.corral@outlook.com>
2023-09-17 22:07:59 -0300

    Primer commit

---
+++ models/server.js
@@ -0,0 +1,53 @@
+
+const express = require('express')
+const cors = require('cors')
+const router = require('../routes/usuarios.routes')
+
+
+
+class Server {
+
+
+    constructor(){
+
+        this.app = express()
+        this.port = process.env.PORT
+        
+        this.usuariosPath = '/api/usuarios'
+        //Middlewares
+        this.middlewares()
+
+        //Rutas de la app
+        this.routes()
+    }x
+    
+
+    middlewares(){
+
+        //CORS
+        this.app.use( cors() )
+        //Lectura parseo del body
+        this.app.use ( express.json() )
+        //Carpeta publica
+        this.app.use(express.static('public'))
+
+    }
+
+    routes(){
+
+        this.app.use(this.usuariosPath,router)
+
+    }
+
+    listen(){
+
+        this.app.listen(this.port,()=>{
+            console.log('run en puerto '+this.port)
+        })
+    }
+}
+
+
+
+
+module.exports = Server
---
+++ routes/usuarios.routes.js
@@ -0,0 +1,19 @@
+
+const { Router } = require('express')
+const { usuariosGet, usuariosPost, usuariosPut, usuariosPatch, usuariosDelete } = require('../controllers/usuarios.controller')
+
+const router = Router()
+
+router.get('/', usuariosGet)
+
+router.put('/:id', usuariosPut)
+
+router.post('/', usuariosPost)
+
+router.patch('/', usuariosPatch)
+
+router.delete('/', usuariosDelete)
+
+
+
+module.exports = router
